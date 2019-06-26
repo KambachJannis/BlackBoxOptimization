c195ff247e305b32a96d501a9ea5baa900062ae2
Philip Schnittkamp <pschnittkamp@gmail.com>
2019-06-26 20:47:08 +0200

    Update some code.

--- Code/Optimization.R
+++ Code/Optimization.R
@@ -19,19 +19,19 @@ testfunc <- function(X, ...){
 }
 
 ### NSGA-II
-results <- ecr::nsga2(fitness.fun=testfunc_help, n.objectives=2, n.dim=3, lower=rep(0,3), upper=rep(1,3), mu=100L, terminators=list(stopOnIters(1000L)))
-plot(results$pareto.front)
+results_nsga2 <- ecr::nsga2(fitness.fun=testfunc_help, n.objectives=2, n.dim=3, lower=rep(0,3), upper=rep(1,3), mu=100L, terminators=list(stopOnIters(1000L)))
+plot(results_nsga2$pareto.front)
 
 ### SMS-EMOA
-results2 <- ecr::smsemoa(fitness.fun=testfunc_help, n.objectives=2, n.dim=3, lower=rep(0,3), upper=rep(1,3), mu=100L, terminators=list(stopOnIters(10000L)))
-plot(results2$pareto.front)
+results_smsemoa <- ecr::smsemoa(fitness.fun=testfunc_help, n.objectives=2, n.dim=3, lower=rep(0,3), upper=rep(1,3), mu=100L, terminators=list(stopOnIters(10000L)))
+plot(results_smsemoa$pareto.front)
 
 ### MOEA/D
-## 1: prepare test problem
+## 1: Prepare test problem
 ZDT1 <- make_vectorized_smoof(prob.name  = "ZDT1",
                               dimensions = 30)
 
-## 2: set input parameters
+## 2: Set input parameters
 problem   <- list(name       = "testfunc",
                   xmin       = rep(0, 30),
                   xmax       = rep(1, 30),
@@ -56,27 +56,26 @@ showpars  <- list(show.iters = "dots",
                   showevery  = 10)
 seed      <- NULL
 
-## 3: run MOEA/D
-out1 <- moead(problem = problem, 
+## 3: Run MOEA/D
+results_moead <- moead(problem = problem, 
               decomp = decomp, aggfun = aggfun, neighbors = neighbors, variation = variation, 
               update = update, constraint = constraint, scaling = scaling, stopcrit = stopcrit,
               showpars = showpars, seed = seed)
 
-## 4: plot results
-plot(out1$Y[,1], out1$Y[,2])
+## 4: Plot results
+plot(results_moead$Y[,1], results_moead$Y[,2])
 
 
-# Test calls
+#### Test calls ####
+samples2D = as.data.frame(expand.grid(seq(0,20,by=1),seq(0,20,by=1)))
+colnames(samples2D)=c("x","y")
+response2D = batch_apirequest(samples2D, 2, "api-test2D")
+samples2D$f = response2D #the function value
 
-samples = as.data.frame(expand.grid(seq(0,20,by=1),seq(0,20,by=1)))
-colnames(samples)=c("x","y")
-response = batch_apirequest(samples, 2, "api-test2D")
-samples$f = response #the function value
-
-# Plotting example (3D plot)
+### Plotting example (3D plot)
 library(plotly)
 
-plot_ly(samples, intensity = ~f,
+plot_ly(samples2D, intensity = ~f,
         colors = colorRamp(c("blue","green", "red"))) %>%
   add_trace(x = ~x, y = ~y, z = ~f, type = 'mesh3d') %>%
   layout(title="Function Landscape Approximation",
@@ -84,3 +83,9 @@ plot_ly(samples, intensity = ~f,
                       yaxis = list(title="Y"),
                       zaxis = list(title="Function Value")))
 
+samples3D = as.data.frame(expand.grid(seq(0,20,by=1),seq(0,20,by=1),seq(0,20,by=1)))
+colnames(samples3D)=c("x","y","z")
+response3D = batch_apirequest(samples, 1, "api-test3D")
+samples3D$f = response3D #the function value
+
+### Models for the 2D-functions
