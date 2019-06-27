af1935ae9925f5c5cdb9710b8f858510232623da
Philip Schnittkamp <pschnittkamp@gmail.com>
2019-06-27 22:13:53 +0200

    Change code to pave the way for making predictions.

--- Code/Optimization.R
+++ Code/Optimization.R
@@ -19,12 +19,12 @@ testfunc <- function(X, ...){
 }
 
 ### NSGA-II
-results_nsga2 <- ecr::nsga2(fitness.fun=testfunc_help, n.objectives=2, n.dim=3, lower=rep(0,3), upper=rep(1,3), mu=100L, terminators=list(stopOnIters(1000L)))
-plot(results_nsga2$pareto.front)
+results.nsga2 <- ecr::nsga2(fitness.fun = testfunc_help, n.objectives = 2, n.dim = 3, lower = rep(0,3), upper = rep(1,3), mu = 100L, terminators = list(stopOnIters(1000L)))
+plot(results.nsga2$pareto.front)
 
 ### SMS-EMOA
-results_smsemoa <- ecr::smsemoa(fitness.fun=testfunc_help, n.objectives=2, n.dim=3, lower=rep(0,3), upper=rep(1,3), mu=100L, terminators=list(stopOnIters(10000L)))
-plot(results_smsemoa$pareto.front)
+results.smsemoa <- ecr::smsemoa(fitness.fun = testfunc_help, n.objectives = 2, n.dim = 3, lower = rep(0,3), upper = rep(1,3), mu = 100L, terminators = list(stopOnIters(10000L)))
+plot(results.smsemoa$pareto.front)
 
 ### MOEA/D
 ## 1: Prepare test problem
@@ -57,25 +57,31 @@ showpars  <- list(show.iters = "dots",
 seed      <- NULL
 
 ## 3: Run MOEA/D
-results_moead <- moead(problem = problem, 
+results.moead <- moead(problem = problem, 
               decomp = decomp, aggfun = aggfun, neighbors = neighbors, variation = variation, 
               update = update, constraint = constraint, scaling = scaling, stopcrit = stopcrit,
               showpars = showpars, seed = seed)
 
 ## 4: Plot results
-plot(results_moead$Y[,1], results_moead$Y[,2])
+plot(results.moead$Y[,1], results.moead$Y[,2])
 
 
 #### Test calls ####
-samples2D = as.data.frame(expand.grid(seq(0,20,by=1),seq(0,20,by=1)))
-colnames(samples2D)=c("x","y")
-response2D = batch_apirequest(samples2D, 2, "api-test2D")
-samples2D$f = response2D #the function value
+### 2D functions
+samples2D_f1 = as.data.frame(expand.grid(seq(0,20,by=1),seq(0,20,by=1)))
+colnames(samples2D_f1)=c("x","y")
+response2D_f1 = batch_apirequest(samples2D_f1, 1, "api-test2D")
+samples2D_f1$f = response2D_f1
 
-### Plotting example (3D plot)
+samples2D_f2 = as.data.frame(expand.grid(seq(0,20,by=1),seq(0,20,by=1)))
+colnames(samples2D_f2)=c("x","y")
+response2D_f2 = batch_apirequest(samples2D_f2, 2, "api-test2D")
+samples2D_f2$f = response2D_f2
+
+## Plotting example (3D plot)
 library(plotly)
 
-plot_ly(samples2D, intensity = ~f,
+plot_ly(samples2D_f1, intensity = ~f,
         colors = colorRamp(c("blue","green", "red"))) %>%
   add_trace(x = ~x, y = ~y, z = ~f, type = 'mesh3d') %>%
   layout(title="Function Landscape Approximation",
@@ -83,9 +89,39 @@ plot_ly(samples2D, intensity = ~f,
                       yaxis = list(title="Y"),
                       zaxis = list(title="Function Value")))
 
-samples3D = as.data.frame(expand.grid(seq(0,20,by=1),seq(0,20,by=1),seq(0,20,by=1)))
-colnames(samples3D)=c("x","y","z")
-response3D = batch_apirequest(samples, 1, "api-test3D")
-samples3D$f = response3D #the function value
+plot_ly(samples2D_f2, intensity = ~f,
+        colors = colorRamp(c("blue","green", "red"))) %>%
+  add_trace(x = ~x, y = ~y, z = ~f, type = 'mesh3d') %>%
+  layout(title="Function Landscape Approximation",
+         scene = list(xaxis = list(title="X"),
+                      yaxis = list(title="Y"),
+                      zaxis = list(title="Function Value")))
+
+### 3D functions
+samples3D_f1 = as.data.frame(expand.grid(seq(0,20,by=2),seq(0,20,by=2),seq(0,20,by=2)))
+colnames(samples3D_f1)=c("x","y","z")
+response3D_f1 = batch_apirequest(samples3D_f1, 1, "api-test3D")
+samples3D_f1$f = response3D_f1
+
+samples3D_f2 = as.data.frame(expand.grid(seq(0,20,by=2),seq(0,20,by=2),seq(0,20,by=2)))
+colnames(samples3D_f2)=c("x","y","z")
+response3D_f2 = batch_apirequest(samples3D_f2, 2, "api-test3D")
+samples3D_f2$f = response3D_f2
 
 ### Models for the 2D-functions
+library(mlr)
+library(e1071)
+## Create the task
+task.2D_f1 <- makeRegrTask(data = samples2D_f1, target = "f")
+
+## Create learner and train model
+lrn.2D_f1 <- makeLearner("regr.svm")
+mdl.svm.2D_f1 <- train(learner = lrn.2D_f1, task = task.2D_f1)
+
+## Make predictions
+new_samples2D_f1 = data.frame(1.5,1.5)
+colnames(new_samples2D_f1)=c("x","y")
+new_samples2D_f1$f = 0
+
+pred.svm.2D_f1 <- predict(object = mdl.svm.2D_f1, newdata = new_samples2D_f1)
+pred.svm.2D_f1$data$response
